35c0180942368dc4dba92c216d024a965cc830c8
7059705091 <verma.sahil3232@gmail.com>
2017-01-17 00:44:00 -0500

    check boxes to radio buttons

--- Assignment1/Form1.Designer.cs
+++ Assignment1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Assignment1
 {
-    partial class Form1
+    partial class Form
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,267 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.Calculate = new System.Windows.Forms.Button();
+            this.Print = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Name = new System.Windows.Forms.Label();
+            this.ID = new System.Windows.Forms.Label();
+            this.Hours = new System.Windows.Forms.Label();
+            this.Sales = new System.Windows.Forms.Label();
+            this.Bonus = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Calculate
+            // 
+            this.Calculate.AutoSize = true;
+            this.Calculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Calculate.Location = new System.Drawing.Point(3, 290);
+            this.Calculate.Margin = new System.Windows.Forms.Padding(4);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(85, 26);
+            this.Calculate.TabIndex = 5;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            // 
+            // Print
+            // 
+            this.Print.AutoSize = true;
+            this.Print.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Print.Location = new System.Drawing.Point(156, 290);
+            this.Print.Margin = new System.Windows.Forms.Padding(4);
+            this.Print.Name = "Print";
+            this.Print.Size = new System.Drawing.Size(69, 26);
+            this.Print.TabIndex = 6;
+            this.Print.Text = "Print";
+            this.Print.UseVisualStyleBackColor = true;
+            this.Print.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.AutoSize = true;
+            this.Clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clear.Location = new System.Drawing.Point(307, 290);
+            this.Clear.Margin = new System.Windows.Forms.Padding(4);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(62, 26);
+            this.Clear.TabIndex = 7;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(156, 104);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 22);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(156, 136);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 22);
+            this.textBox2.TabIndex = 9;
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name.Location = new System.Drawing.Point(36, 107);
+            this.Name.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Name.Name = "Name";
+            this.Name.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Name.Size = new System.Drawing.Size(120, 16);
+            this.Name.TabIndex = 10;
+            this.Name.Text = "Employee\'s Name";
+            this.Name.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ID.Location = new System.Drawing.Point(67, 139);
+            this.ID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(89, 16);
+            this.ID.TabIndex = 11;
+            this.ID.Text = "Employee ID:";
+            // 
+            // Hours
+            // 
+            this.Hours.AutoSize = true;
+            this.Hours.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Hours.Location = new System.Drawing.Point(58, 188);
+            this.Hours.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Hours.Name = "Hours";
+            this.Hours.Size = new System.Drawing.Size(98, 16);
+            this.Hours.TabIndex = 12;
+            this.Hours.Text = "Hours Worked:";
+            this.Hours.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Sales
+            // 
+            this.Sales.AutoSize = true;
+            this.Sales.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sales.Location = new System.Drawing.Point(76, 223);
+            this.Sales.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Sales.Name = "Sales";
+            this.Sales.Size = new System.Drawing.Size(80, 16);
+            this.Sales.TabIndex = 13;
+            this.Sales.Text = "Total Sales:";
+            this.Sales.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Bonus
+            // 
+            this.Bonus.AutoSize = true;
+            this.Bonus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bonus.Location = new System.Drawing.Point(69, 254);
+            this.Bonus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Bonus.Name = "Bonus";
+            this.Bonus.Size = new System.Drawing.Size(87, 16);
+            this.Bonus.TabIndex = 14;
+            this.Bonus.Text = "Sales Bonus:";
+            this.Bonus.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(156, 185);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(150, 22);
+            this.textBox3.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(156, 220);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(150, 22);
+            this.textBox4.TabIndex = 16;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(156, 251);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(150, 22);
+            this.textBox5.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(175, 17);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 16);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Language";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(131, 77);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(178, 36);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(70, 20);
+            this.radioButton1.TabIndex = 24;
+            this.radioButton1.Text = "English";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(178, 62);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(78, 20);
+            this.radioButton2.TabIndex = 25;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Français";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(373, 322);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Bonus);
+            this.Controls.Add(this.Sales);
+            this.Controls.Add(this.Hours);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Print);
+            this.Controls.Add(this.Calculate);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Button Print;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label Name;
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.Label Hours;
+        private System.Windows.Forms.Label Sales;
+        private System.Windows.Forms.Label Bonus;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
 
--- Assignment1/Form1.cs
+++ Assignment1/Form1.cs
@@ -10,11 +10,76 @@ using System.Windows.Forms;
 
 namespace Assignment1
 {
-    public partial class Form1 : Form
+    public partial class Form : System.Windows.Forms.Form
     {
-        public Form1()
+        public Form()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
